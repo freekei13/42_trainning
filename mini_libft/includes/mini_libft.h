7ce86318b034f6f8ee42415f1fbd0f294bd2b3b5
freekei13 <freesamakkasetkan@hotmail.com>
2025-07-21 13:02:48 +0200

    1st commit

---
+++ mini_libft/includes/mini_libft.h
@@ -0,0 +1,13 @@
+#ifndef MINI_LIBFT_H
+#define MINI_LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_atoi(const char *str);
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
---
+++ mini_libft/main.c
@@ -0,0 +1,8 @@
+#include "includes/mini_libft.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("ft_strlen: %zu\n", ft_strlen("Hello, 42!"));
+	return 0;
+}
