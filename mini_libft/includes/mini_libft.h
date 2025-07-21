4afa5620464abb4e1d7b5dd2e27f23748a34cf9d
freekei13 <freesamakkasetkan@hotmail.com>
2025-07-21 16:32:30 +0200

    all ft_ done

--- mini_libft/includes/mini_libft.h
+++ mini_libft/includes/mini_libft.h
@@ -3,9 +3,10 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stddef.h>
 
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *str);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_atoi(const char *str);
 void	*ft_memset(void *b, int c, size_t len);
--- mini_libft/main.c
+++ mini_libft/main.c
@@ -4,5 +4,11 @@
 int main(void)
 {
 	printf("ft_strlen: %zu\n", ft_strlen("Hello, 42!"));
+	printf("ft_strdup: %s\n", ft_strdup("Hello, 42!"));
+	printf("ft_strcmp: %d %d\n", ft_strcmp("Hello, World!", "Hello, 42!"), ft_strcmp("Hello, 42!", "Hello, 42!"));
+	printf("ft_atoi: %d\n", ft_atoi("       ---++---131629kdiHdf"));
+	char buffer[10];
+	ft_memset(buffer, '*', 10);
+	printf("ft_memset: %s\n", buffer);
 	return 0;
 }
