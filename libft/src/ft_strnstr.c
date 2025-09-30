c34e2238879d966d3164e271c7d9a085983d05b7
freekei13 <freesamakkasetkan@hotmail.com>
2025-09-30 17:44:24 +0200

    Update

---
+++ libft/src/ft_strnstr.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csamakka <csamakka@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 15:33:10 by csamakka          #+#    #+#             */
+/*   Updated: 2025/09/20 15:41:02 by csamakka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	if (needle == NULL || needle == "")
+		return	(haystack);
+	if (haystack != NULL)
+	{
+		
