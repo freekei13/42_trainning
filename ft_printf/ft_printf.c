daec2965b06fedf0de1930a8463929bddb426217
freekei13 <freesamakkasetkan@hotmail.com>
2025-10-20 21:37:07 +0200

    Update Makefile, ft_printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: freekei <freekei@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 13:21:51 by freekei           #+#    #+#             */
-/*   Updated: 2025/10/20 19:35:57 by freekei          ###   ########.fr       */
+/*   Updated: 2025/10/20 21:31:31 by freekei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,14 @@ int ft_arguments(va_list args, char type)
 
     len = 0;
     if (type == 'c')
-        len += ft_print_char(va_arg(args, char));
+        len += ft_print_char(va_arg(args, int));
     else if (type == 's')
         len += ft_print_str(va_arg(args, char *));
+    else if (type == 'p')
+        len += ft_print_ptr(va_arg(args, void *));
+    else if (type == 'd' || type == 'i')
+        len += ft_print_nbr(va_arg(args, long));
+    return (len);
 }
 
 int ft_printf(const char *format, ...)
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: freekei <freekei@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 13:42:33 by freekei           #+#    #+#             */
-/*   Updated: 2025/10/18 14:20:46 by freekei          ###   ########.fr       */
+/*   Updated: 2025/10/20 21:18:32 by freekei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define FT_PRINTF_H
 
 # include   <stdarg.h>
+# include	<stddef.h>
+# include	<stdlib.h>
+# include	<unistd.h>
+
+void	ft_putchar(char c);
+int		ft_print_char(int arg);
+int		ft_print_str(char *arg);
+int		ft_print_nbr(long nb);
+int		ft_print_hex(unsigned long long arg, char c);
+int		ft_print_ptr(void *arg);
 
 #endif
