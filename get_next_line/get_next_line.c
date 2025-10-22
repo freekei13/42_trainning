053fbd28ce87b0463508f6d277f2bea738e376a4
freekei13 <freesamakkasetkan@hotmail.com>
2025-10-22 15:03:04 +0200

    get_next_line study, bon avancement

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,12 +1,36 @@
 #include "get_next_line.h"
 
+char	*fill_line_left_line(int fd, char *buffer);
+
 char	*get_next_line(int fd)
 {
+	static char	*left_lines;
 	char	*buffer;
+	char	*line;
 
-	buffer = malloc(5);
-	read(fd, buffer, 5);
-	return (buffer);
+	buffer = malloc(BUFFER_SIZE * sizeof(char) + 1);
+	if (fd < 0 || BUFFER_SIZE < 0)
+	{
+		free(buffer);
+		buffer = NULL;
+		free(left_lines);
+		left_lines = NULL;
+		return (NULL);
+	}
+	if (!buffer)
+		return (NULL);
+	line = fill_line_left_line(fd, buffer);
+	return (line);
+}
+
+char	*fill_line_left_line(int fd, char *buffer)
+{
+	char	*line;
+
+	read(fd, buffer, BUFFER_SIZE);
+	line = ft_strdup(buffer);
+	free(buffer);
+	return (line);
 }
 #include <stdio.h>
 
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,1 +1,29 @@
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	i;
+
+	i = 0;
+	dup = malloc(ft_strlen(s) + 1);
+	if (!dup)
+		return (NULL);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = 0;
+	return (dup);
+}
