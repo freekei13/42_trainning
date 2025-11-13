faacd2cfbac2a7b70f3d503040facba90412cf7a
freekei13 <freesamakkasetkan@hotmail.com>
2025-11-13 16:27:16 +0100

    little update Makefile

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -3,11 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csamakka <csamakka@student.42.fr>          +#+  +:+       +#+        */
+/*   By: freekei <freekei@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 11:28:17 by csamakka          #+#    #+#             */
-/*   Updated: 2025/11/12 14:12:12 by csamakka         ###   ########.fr       */
+/*   Updated: 2025/11/13 16:17:46 by freekei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	char	**arg;
+	int		i;
+	
+	if (argc < 2)
+		return (0);
+	if (argc == 2)
+		arg = ft_split(argv[1], 32);
+	i = 0;
+	while (arg[i++])
+		ft_printf("%s\n", arg[i]);
+	return (0);
+}
--- push_swap/src/push_swap.h
+++ push_swap/src/push_swap.h
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csamakka <csamakka@student.42.fr>          +#+  +:+       +#+        */
+/*   By: freekei <freekei@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 11:28:20 by csamakka          #+#    #+#             */
-/*   Updated: 2025/11/11 11:45:39 by csamakka         ###   ########.fr       */
+/*   Updated: 2025/11/13 15:59:23 by freekei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef		PUSH_SWAP_H
 # define	PUSH_SWAP_H
 # include	<limits.h>
-
+# include	"../libft/libft.h"
+# include	"../ft_printf/ft_printf.h"
 #endif
