85dd59113902fe3c73a49848ccfe4a9f59bb39c1
Chetsada Samakkasetkan <csamakka@c1r17s3.42lausanne.ch>
2025-11-17 16:34:49 +0100

    push_swap update

--- push_swap/src/input_check.c
+++ push_swap/src/input_check.c
@@ -1,14 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   input_check.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: csamakka <csamakka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/11 11:28:17 by csamakka          #+#    #+#             */
-/*   Updated: 2025/11/17 11:18:29 by csamakka         ###   ########.fr       */
+/*   Created: 2025/11/17 11:20:09 by csamakka          #+#    #+#             */
+/*   Updated: 2025/11/17 16:12:13 by csamakka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	error_check(char **inputs)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (inputs[i])
+	{
+		j = 0;
+		while (inputs[i][j])
+		{
+			if (ft_isdigit(inputs[i][j]) == 0)
+				return (ft_printf("ERROR"), 0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+char	**input_check(int ac, char *av[])
+{
+	char	**arg;
+	
+	if (ac < 2)
+		return (0);
+	if (ac == 2)
+		arg = ft_split(av[1], 32);
+	else
+		arg = av;
+	return (arg);
+}
--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -1,21 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: csamakka <csamakka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/11 11:28:20 by csamakka          #+#    #+#             */
-/*   Updated: 2025/11/17 15:59:46 by csamakka         ###   ########.fr       */
+/*   Created: 2025/11/17 11:18:40 by csamakka          #+#    #+#             */
+/*   Updated: 2025/11/17 16:00:36 by csamakka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		PUSH_SWAP_H
-# define	PUSH_SWAP_H
-# include	<limits.h>
-# include	"../libft/libft.h"
-# include	"../ft_printf/ft_printf.h"
+#include "push_swap.h"
 
-char	**input_check(int ac, char *av[]);
-int		error_check(char **inputs);
-#endif
+int	main(int argc, char **argv)
+{
+	char	**arg;
+	int		i;
+	
+	arg = input_check(argc, argv);
+	if (error_check(arg) == 0)
+		return (0);
+	i = 0;
+	while (arg[i])
+	{
+		ft_printf("%s\n", arg[i]);
+		i++;
+	}
+}
