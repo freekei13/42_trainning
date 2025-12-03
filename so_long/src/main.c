b4fe47d65b04ce1b46d41a225d248fb673097300
freekei13 <freesamakkasetkan@hotmail.com>
2025-12-03 03:15:00 +0100

    so_long start, display window and a pixel

---
+++ so_long/src/main.c
@@ -0,0 +1,29 @@
+#include <mlx.h>
+
+typedef struct	s_data 
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	mlx_pixel_put(mlx, mlx_win, 50, 50, 0x00FF0000);
+	mlx_loop(mlx);
+}
