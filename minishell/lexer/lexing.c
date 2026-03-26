6d473066ce80fc984efd99fd7486900b081838cd
freekei13 <freesamakkasetkan@hotmail.com>
2026-03-26 15:02:27 +0100

    minishell lexer token: done

--- minishell/lexer/lexing.c
+++ minishell/lexer/lexing.c
@@ -5,8 +5,12 @@ t_token	*lexer(char *line)
 	t_token	*tokens;
 	int		i;
 	int		counter;
+	int		single_q;
+	int		double_q;
 	char	*word;
 
+	single_q = 0;
+	double_q = 0;
 	tokens = NULL;
 	i = 0;
 	while (line[i])
@@ -47,38 +51,30 @@ t_token	*lexer(char *line)
 		else
 		{
 			counter = i;
-			if (line[i] == '\"')
+			while ((line[counter] != ' ' && line[counter] != '|' && line[counter] != '<' && line[counter] != '>') || (double_q == 1 || single_q == 1))
 			{
+				if (!line[counter])
+					break ;
+				if (line[counter] == '\"')
+				{
+					if (double_q != 0)
+						double_q = 0;
+					else
+						double_q = 1;
+				}
+				if (line[counter] == '\'')
+				{
+					if (single_q != 0)
+						single_q = 0;
+					else
+						single_q = 1;
+				}
 				counter++;
-				while (line[counter] != '\"' && line[counter])
-				{
-					counter++;
-				}
-				word = ft_substr(line, i, counter + 1 - i);
-				add_token_back(&tokens, new_token(word, WORD));
-				i = counter + 1;
 			}
-			else if (line[i] == '\'')
-			{
-				counter++;
-				while (line[counter] != '\'' && line[counter])
-				{
-					counter++;
-				}
-				word = ft_substr(line, i, counter + 1 - i);
-				add_token_back(&tokens, new_token(word, WORD));
-				i = counter + 1;
-			}
-			else
-			{
-				while (line[counter] != ' ' && line[counter] != '|' && line[counter] != '>' && line[counter] != '<' && line[counter])
-				{
-					counter++;
-				}
-				word = ft_substr(line, i, counter - i);
-				add_token_back(&tokens, new_token(word, WORD));
-				i = counter;
-			}
+			word = ft_substr(line, i, counter - i);
+			add_token_back(&tokens, new_token(word, WORD));
+			free(word);
+			i = counter;
 		}
 	}
 	return (tokens);
